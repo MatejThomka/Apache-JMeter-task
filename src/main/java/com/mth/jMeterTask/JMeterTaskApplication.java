4f31c1e7209a1c689f6d87fa657dfde879cb3bb1
Matej Thomka <matej.thomka.eleinst@gmail.com>
2023-11-28 18:20:33 +0100

    Import Spring boot start up files.

---
+++ src/main/java/com/mth/jMeterTask/JMeterTaskApplication.java
@@ -0,0 +1,13 @@
+package com.mth.jMeterTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JMeterTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JMeterTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mth/jMeterTask/JMeterTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mth.jMeterTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JMeterTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
