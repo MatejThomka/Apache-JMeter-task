319f8cf3a384179c42625969bc1f7590bc2c410f
Matej Thomka <matej.thomka.eleinst@gmail.com>
2023-11-30 12:57:46 +0100

    Change parameters for searching method.

--- src/main/java/com/mth/jMeterTask/services/TestPersonService.java
+++ src/main/java/com/mth/jMeterTask/services/TestPersonService.java
@@ -2,11 +2,12 @@ package com.mth.jMeterTask.services;
 
 import com.mth.jMeterTask.exceptions.JMeterException;
 import com.mth.jMeterTask.models.TestPerson;
+import java.util.List;
 
 public interface TestPersonService {
 
   TestPerson detail(Long id) throws JMeterException;
-  TestPerson search(Long id, String name, String lastname, String date) throws JMeterException;
+  List<TestPerson> search(TestPerson testPerson) throws JMeterException;
   TestPerson update(Long id, TestPerson testPerson) throws JMeterException;
   void create(String name, String lastname, String birthNumber) throws JMeterException;
 }
