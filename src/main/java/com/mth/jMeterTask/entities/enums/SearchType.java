e1ad20d429ade76c8e2c92abe9fdd6b1a4b1ff3a
Matej Thomka <matej.thomka.eleinst@gmail.com>
2023-11-30 21:30:48 +0100

    Clean code.

---
+++ src/main/java/com/mth/jMeterTask/entities/enums/SearchType.java
@@ -0,0 +1,6 @@
+package com.mth.jMeterTask.entities.enums;
+
+public enum SearchType {
+  BY_ID, BY_NAME, BY_LASTNAME, BY_DATE, BY_NAME_AND_LASTNAME, BY_NAME_AND_DATE,
+  BY_LASTNAME_AND_DATE, BY_NAME_AND_LASTNAME_AND_DATE
+}
