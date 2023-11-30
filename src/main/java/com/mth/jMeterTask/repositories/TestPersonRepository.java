af89fc21c21103edaf69839ea97089502e4b4e25
Matej Thomka <matej.thomka.eleinst@gmail.com>
2023-11-30 12:57:24 +0100

    Create JPQL methods for searching in repository

--- src/main/java/com/mth/jMeterTask/repositories/TestPersonRepository.java
+++ src/main/java/com/mth/jMeterTask/repositories/TestPersonRepository.java
@@ -2,13 +2,18 @@ package com.mth.jMeterTask.repositories;
 
 import com.mth.jMeterTask.models.TestPerson;
 import java.util.List;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TestPersonRepository extends JpaRepository<TestPerson, Integer> {
 
   TestPerson findById(Long id);
-  List<TestPerson> findByNameStartingWith(String namePrefix);
-  List<TestPerson> findByLastnameStartingWith(String lastnamePrefix);
+  List<TestPerson> findAllByNameStartingWith(String name);
+  List<TestPerson> findAllByLastnameStartingWith(String lastname);
+  List<TestPerson> findAllByNameStartingWithAndLastnameStartingWith(String name, String lastname);
+  List<TestPerson> findAllByBirthNumberContaining(String yearMonthDay);
 }
+
