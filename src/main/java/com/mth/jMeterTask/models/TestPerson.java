419def2035e0724bb743ad938b66741f420de623
Matej Thomka <matej.thomka.eleinst@gmail.com>
2023-11-30 17:01:58 +0100

    Change @Data to @Getter and @Setter.

--- src/main/java/com/mth/jMeterTask/models/TestPerson.java
+++ src/main/java/com/mth/jMeterTask/models/TestPerson.java
@@ -10,12 +10,14 @@ import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 
 @Entity
-@Data
+@Getter
+@Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
