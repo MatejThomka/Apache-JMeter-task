70c1e278e24a1f6a2aaa53d2a6210b084443b81f
Matej Thomka <matej.thomka.eleinst@gmail.com>
2023-11-30 13:14:22 +0100

    Update creating of persons for repository.

--- src/main/java/com/mth/jMeterTask/JMeterTaskApplication.java
+++ src/main/java/com/mth/jMeterTask/JMeterTaskApplication.java
@@ -1,5 +1,8 @@
 package com.mth.jMeterTask;
 
+import static com.mth.jMeterTask.models.enums.Gender.FEMALE;
+import static com.mth.jMeterTask.models.enums.Gender.MALE;
+
 import com.mth.jMeterTask.exceptions.JMeterException;
 import com.mth.jMeterTask.models.TestPerson;
 import com.mth.jMeterTask.services.TestPersonService;
@@ -23,26 +26,26 @@ public class JMeterTaskApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws JMeterException {
 		List<TestPerson> listOfPerson = new ArrayList<>();
-		TestPerson testPerson = new TestPerson("Martin", "Koala", "941122/8140");
-		TestPerson testPerson1 = new TestPerson("Milan", "Vevericka", "981225/8740");
-		TestPerson testPerson2 = new TestPerson("Peter", "Suchy", "521126/118");
-		TestPerson testPerson3 = new TestPerson("Marian", "Tenky", "930514/8020");
-		TestPerson testPerson4 = new TestPerson("Michal", "Kolac", "990316/8851");
-		TestPerson testPerson5 = new TestPerson("Eva", "Kralova", "885914/7856");
-		TestPerson testPerson6 = new TestPerson("Martina", "Koalova", "941207/8148");
-		TestPerson testPerson7 = new TestPerson("Ladislav", "Koral", "770925/4235");
-		TestPerson testPerson8 = new TestPerson("Ondrej", "Jezko", "550420/156");
-		TestPerson testPerson9 = new TestPerson("Bohuslav", "Finsky", "840310/7512");
-		TestPerson testPerson10 = new TestPerson("Zuzana", "Pomala", "035715/9125");
-		TestPerson testPerson11 = new TestPerson("Kristina", "Rychla", "005523/9017");
-		TestPerson testPerson12 = new TestPerson("Kristina", "Hruba", "975217/8447");
-		TestPerson testPerson13 = new TestPerson("Tibor", "Mrkva", "821005/7534");
-		TestPerson testPerson14 = new TestPerson("Dominika", "Mrkvova", "051228/9520");
-		TestPerson testPerson15 = new TestPerson("Marcel", "Dreveny", "940411/8059");
-		TestPerson testPerson16 = new TestPerson("Lukas", "Kamen", "710514/4125");
-		TestPerson testPerson17 = new TestPerson("Kamil", "Lokomotiva", "620822/1254");
-		TestPerson testPerson18 = new TestPerson("Peter", "Doska", "661005/1536");
-		TestPerson testPerson19 = new TestPerson("Michal", "Mrak", "100725/9851");
+		TestPerson testPerson = new TestPerson("Martin", "Koala", "941122/8140", MALE);
+		TestPerson testPerson1 = new TestPerson("Milan", "Vevericka", "981225/8740", MALE);
+		TestPerson testPerson2 = new TestPerson("Peter", "Suchy", "521126/118", MALE);
+		TestPerson testPerson3 = new TestPerson("Marian", "Tenky", "930514/8020", MALE);
+		TestPerson testPerson4 = new TestPerson("Michal", "Kolac", "990316/8851", MALE);
+		TestPerson testPerson5 = new TestPerson("Eva", "Kralova", "885914/7856", FEMALE);
+		TestPerson testPerson6 = new TestPerson("Martina", "Koalova", "941207/8148", FEMALE);
+		TestPerson testPerson7 = new TestPerson("Ladislav", "Koral", "770925/4235", MALE);
+		TestPerson testPerson8 = new TestPerson("Ondrej", "Jezko", "550420/156", MALE);
+		TestPerson testPerson9 = new TestPerson("Bohuslav", "Finsky", "840310/7512", MALE);
+		TestPerson testPerson10 = new TestPerson("Zuzana", "Pomala", "035715/9125", FEMALE);
+		TestPerson testPerson11 = new TestPerson("Kristina", "Rychla", "005523/9017", FEMALE);
+		TestPerson testPerson12 = new TestPerson("Kristina", "Hruba", "975217/8447", FEMALE);
+		TestPerson testPerson13 = new TestPerson("Tibor", "Mrkva", "821005/7534", MALE);
+		TestPerson testPerson14 = new TestPerson("Dominika", "Mrkvova", "051228/9520", FEMALE);
+		TestPerson testPerson15 = new TestPerson("Marcel", "Dreveny", "940411/8059", MALE);
+		TestPerson testPerson16 = new TestPerson("Lukas", "Kamen", "710514/4125", MALE);
+		TestPerson testPerson17 = new TestPerson("Kamil", "Lokomotiva", "620822/1254", MALE);
+		TestPerson testPerson18 = new TestPerson("Peter", "Doska", "661005/1536", MALE);
+		TestPerson testPerson19 = new TestPerson("Michal", "Mrak", "100725/9851", MALE);
 
 		listOfPerson.add(testPerson);
 		listOfPerson.add(testPerson1);
