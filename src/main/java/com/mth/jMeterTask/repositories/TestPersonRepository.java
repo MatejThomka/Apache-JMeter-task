3e564718f05eb63072264f05d5c1b58a0b6e166b
Matej Thomka <matej.thomka.eleinst@gmail.com>
2023-12-02 23:13:00 +0100

    Change JPQL for findById in database to Optional and add NonNull annotation.

--- src/main/java/com/mth/jMeterTask/repositories/TestPersonRepository.java
+++ src/main/java/com/mth/jMeterTask/repositories/TestPersonRepository.java
@@ -2,13 +2,15 @@ package com.mth.jMeterTask.repositories;
 
 import com.mth.jMeterTask.entities.TestPerson;
 import java.util.List;
+import java.util.Optional;
+import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TestPersonRepository extends JpaRepository<TestPerson, Integer> {
 
-  TestPerson findById(Long id);
+  @NonNull Optional<TestPerson> findById(@NonNull Integer id);
 
   List<TestPerson> findAllByNameStartingWith(String name);
 
