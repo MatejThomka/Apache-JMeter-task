0931d848f6ea9e358e9a076079beb1cd0136f6ec
Matej Thomka <matej.thomka.eleinst@gmail.com>
2023-12-02 23:11:38 +0100

    Create new exception for not found test person in database.

---
+++ src/main/java/com/mth/jMeterTask/exceptions/TestPersonNotFoundException.java
@@ -0,0 +1,8 @@
+package com.mth.jMeterTask.exceptions;
+
+public class TestPersonNotFoundException extends RuntimeException {
+
+  public TestPersonNotFoundException(String message) {
+    super(message);
+  }
+}
