b714dd5c972f6742e81f62aa821067d4fccdb2b7
Matej Thomka <matej.thomka.eleinst@gmail.com>
2023-12-02 23:09:44 +0100

    Delete unused exception.

--- src/main/java/com/mth/jMeterTask/exceptions/JMeterException.java
+++ src/main/java/com/mth/jMeterTask/exceptions/JMeterException.java
@@ -5,11 +5,6 @@ import org.springframework.http.HttpStatus;
 public class JMeterException extends Exception {
   HttpStatus httpStatus;
 
-  public JMeterException(String message) {
-    super(message);
-    httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
-  }
-
   public JMeterException(String message, HttpStatus status) {
     super(message);
     this.httpStatus = status;
