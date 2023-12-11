86b0cfaa48c98af3aa17f2b7725f020cc3fec7c9
Matej Thomka <matej.thomka.eleinst@gmail.com>
2023-12-11 23:55:21 +0100

    Change exception.

--- src/main/java/com/mth/jMeterTask/exceptions/BirthNumberException.java
+++ src/main/java/com/mth/jMeterTask/exceptions/BirthNumberException.java
@@ -1,10 +1,9 @@
 package com.mth.jMeterTask.exceptions;
 
-import org.springframework.http.HttpStatus;
 
 public class BirthNumberException extends JMeterException {
 
   public BirthNumberException(String message) {
-    super(message, HttpStatus.BAD_REQUEST);
+    super(message);
   }
 }
